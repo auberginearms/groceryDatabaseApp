db49e2a731d23e491395235c649b063cc23da78d
auberginearms <a.mia.oh@gmail.com>
2023-10-19 19:11:43 +1100

    refactored credentialsAreValid

--- server/getCredentials.ts
+++ server/getCredentials.ts
@@ -2,25 +2,25 @@
 
 import { Credentials } from "@/app/types";
 
+const validLoginDetails: Credentials[] = [
+  {
+    username: "FakeUser1",
+    password: "Password1",
+  },
+  {
+    username: "FakeUser2",
+    password: "Password2",
+  },
+];
+
 export async function credentialsAreValid(
   inputUsername: string,
   inputPassword: string
 ) {
-  const validLoginDetails: Credentials[] = [
-    {
-      username: "FakeUser1",
-      password: "Password1",
-    },
-    {
-      username: "FakeUser2",
-      password: "Password2",
-    },
-  ];
-  const elementWithMatchingUsername = validLoginDetails.find((loginDetails) => {
-    return loginDetails.username === inputUsername;
+  return validLoginDetails.some((loginDetails, index) => {
+    return (
+      loginDetails.username === inputUsername &&
+      validLoginDetails[index].password === inputPassword
+    );
   });
-  if (!elementWithMatchingUsername) {
-    return;
-  }
-  return elementWithMatchingUsername.password === inputPassword;
 }
