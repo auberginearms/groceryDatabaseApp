af12b81a211058fa794bea8a0be303d6b06a1b60
Mia Oh <a.mia.oh@gmail.com>
2023-11-01 10:13:49 +1100

    removed any type

--- app/Login.tsx
+++ app/Login.tsx
@@ -31,7 +31,7 @@ export function Login(props: {
             type="text"
             placeholder="enter your username"
             value={username}
-            onChange={(e:any) => {
+            onChange={(e) => {
               const newUsername = e.target.value;
               setUsername(newUsername);
             }}
@@ -45,7 +45,7 @@ export function Login(props: {
             type="password"
             placeholder="enter your password"
             value={password}
-            onChange={(e:any) => {
+            onChange={(e) => {
               const newPassword = e.target.value;
               setPassword(newPassword);
             }}
